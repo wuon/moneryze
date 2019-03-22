3640974ba8d69e608f73c66181ab7a9fecec54ff
Katung Wu <wuon@protonmail.com>
2019-03-22 18:38:14 -0400

    resAddCC

--- examples/pay.js
+++ examples/pay.js
@@ -1,30 +1,48 @@
-'use strict'
-
 const moneris = require('../index');
 
 moneris.init({
   app_name: 'Test',
   store_id: 'store5',
   api_token: 'yesguy',
+  crypt_type: '7',
   test: true,
 });
 
-moneris.purchaseCC({
-    amount: (5.99*2), // we handle formatting for you, you just do your math.
-    card: '4242424242424242',
-    expiry: '20/11',
-    description: 'Two drinks',
-    //forceDecline: true, //uncomment this if you want to test declined card (test must be equal to true in credentials)
-}).then((result)=>{ //APPROVED
-  result.raw = undefined;//comment out if you want the raw response.
+
+moneris.resAddCC({
+  pan: '4242424242424242',
+  expdate: '2011',
+}).then((result) => {
   console.log('--');
   console.log('Clean Response (passed):');
-  console.log(result)
+  console.log(result);
 })
-.catch((err)=>{ //DECLINED
-  //err.raw = undefined;//comment out if you want the raw response.
+  .catch((err) => { // DECLINED
+    // err.raw = undefined;//comment out if you want the raw response.
+    console.log('--');
+    console.log('Clean Response (failed):');
+    console.log(err);
+    // console.log(err.raw);
+  });
+
+
+/*
+moneris.purchaseCC({
+  amount: (5.99 * 2), // we handle formatting for you, you just do your math.
+  card: '4242424242424242',
+  expiry: '20/11',
+  description: 'Two drinks',
+  // forceDecline: true, //uncomment this if you want to test declined card (test must be equal to true in credentials)
+}).then((result) => { // APPROVED
   console.log('--');
-  console.log('Clean Response (failed):');
-  console.log(err);
-  //console.log(err.raw);
+  console.log('Clean Response (passed):');
+  console.log(result.msg);
 })
+  .catch((err) => { // DECLINED
+  // err.raw = undefined;//comment out if you want the raw response.
+    console.log('--');
+    console.log('Clean Response (failed):');
+    console.log(err);
+  // console.log(err.raw);
+  });
+*/
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const xml = require('xml2js');
-const request = require('request-promise');
 const Promise = require('bluebird');
 const numeral = require('numeral');
+const axios = require('axios');
 
 Promise.promisifyAll(xml);
 
@@ -28,7 +28,6 @@ const cleanse = (str, spaces) => {
     .trim() : '');
 };
 
-// Check out https://developer.moneris.com/More/Testing/Penny%20Value%20Simulator, for more "magic" amounts to force different responses.
 /* eslint-disable-next-line no-nested-ternary */
 const firstElement = (arr, assertion) => (Array.isArray(arr) && arr.length > 0
   && arr[0] ? (assertion ? arr[0] === assertion : arr[0]) : null);
@@ -40,57 +39,56 @@ const normalizeExpiry = (format, expiry) => {
   return expiry;
 };
 
-module.exports.init = (configuration) => {
-  config = configuration;
-  if (config.country_code) {
-    config.country_code = config.country_code.toUpperCase();
-    if (config.country_code !== 'CA'
-      && !Object.prototype.hasOwnProperty.call(globals, `${config.country_code}_HOST`)) {
-      return Promise.reject(new TypeError('Invalid country code'));
-    }
+const format = (data) => {
+  const fe = firstElement; // shorthand
+  const code = fe(data.ResponseCode);
+  const status = {
+    msg: cleanse(fe(data.Message)),
+    code,
+    reference: fe(data.ReferenceNum),
+    iso: fe(data.ISO),
+    receipt: fe(data.ReceiptId),
+    isVisa: fe(data.CardType, 'V'),
+    isMasterCard: fe(data.CardType, 'M'),
+    isVisaDebit: fe(data.IsVisaDebit, 'true'),
+    authCode: fe(data.AuthCode),
+    timeout: fe(data.TimedOut, 'true'),
+    date: fe(data.TransDate),
+    time: fe(data.TransTime),
+    dataKey: fe(data.DataKey),
+  };
+  const approved = !status.timeout && ((code) === '00' || code ? parseInt(code, 10) < 50 : false);
+  if (approved) {
+    return status;
   }
-  return Promise.resolve();
+  return {
+    code: status.code,
+    msg: (status.timeout ? 'TIMEOUT' : status.msg) || 'DECLINED',
+    raw: data,
+  };
 };
 
-module.exports.purchaseCC = (data) => {
+const send = async (data, type) => {
   if (!config || !config.store_id || !config.api_token) {
     return Promise.reject(new Error('configuration not initialized'));
   }
-  const pan = cleanse(data.card, true);
-  // Moneris takes expiry date format YY/MM, however most websites use the MM/YY format, you can specify this in a verbose manner in the configuration by setting configuration.expiryFormat = 'MM/YY'; or the default 'YY/MM';
-  const expdate = normalizeExpiry(config.expiryFormat, cleanse(data.expiry, true));
-  let { amount } = data;
-  // Moneris is super picky about formatting..
-  if (data.forceDecline && config.test) {
-    amount = 0.05;
+  const out = {};
+  out.crypt_type = data.crypt_type || config.crypt_type;
+  if (data.pan) {
+    out.pan = cleanse(data.pan, true);
   }
-  amount = (amount ? numeral(amount).format('0.00') : false);
-  const suffix = `${(new Date()).getTime()}-${Math.ceil(Math.random() * 10000)}`;
-  /* eslint-disable camelcase */
-  const order_id = data.order_id || `${cleanse(config.app_name, true)}-Purchase-${suffix}`;
-  const cust_id = data.cust_id || `customer-${suffix}`;
-  const dynamic_descriptor = data.description || data.dynamic_descriptor || 'purchase';
-  /* eslint-enable camelcase */
-  const req = {
-    type: 'purchase',
-    cust_id,
-    order_id,
-    amount,
-    pan,
-    expdate,
-    crypt_type: 7,
-    dynamic_descriptor,
-    status_check: false,
-  };
+  if (data.expdate) {
+    out.expdate = normalizeExpiry(config.expiryFormat, cleanse(data.expdate, true));
+  }
   const body = {
     store_id: config.store_id,
     api_token: config.api_token,
   };
-  if (req.type === 'attribute_query' || req.type === 'session_query') {
+  if (type === 'attribute_query' || type === 'session_query') {
     body.risk = {};
-    body.risk[req.type] = req;
+    body.risk[type] = out;
   } else {
-    body[req.type] = req;
+    body[type] = out;
   }
   let prefix = '';
   if (!!config.country_code && config.country_code !== 'CA') {
@@ -101,50 +99,38 @@ module.exports.purchaseCC = (data) => {
   if (config.test) {
     hostPrefix += 'TEST_';
   }
-  if (req.type === 'acs' || req.type === 'txn') {
+  if (type === 'acs' || type === 'txn') {
     filePrefix += 'MPI_';
   }
   const options = {
-    uri: `${globals.PROTOCOL}://${globals[`${hostPrefix}HOST`]}:${globals.PORT}${globals[`${filePrefix}FILE`]}`,
+    url: `${globals.PROTOCOL}://${globals[`${hostPrefix}HOST`]}:${globals.PORT}${globals[`${filePrefix}FILE`]}`,
     method: 'POST',
-    body: xmlBuilder.buildObject(body),
+    data: xmlBuilder.buildObject(body),
     headers: {
-      'User-Agent': globals.API_VERSION,
+      'USER-AGENT': globals.API_VERSION,
+      'CONTENT-TYPE': 'text/xml',
     },
     timeout: globals.CLIENT_TIMEOUT * 1000,
   };
-  return request(options)
-    .then(res => xml.parseStringAsync(res))
-    .then(res => (Array.isArray(res.response.receipt) ? res.response.receipt[0] : res.response.receipt))
-    .then((result) => {
-      console.log(result);
-      const fe = firstElement; // shorthand
-      const code = fe(result.ResponseCode);
-      const status = {
-        msg: cleanse(fe(result.Message)),
-        code,
-        reference: fe(result.ReferenceNum),
-        iso: fe(result.ISO),
-        receipt: fe(result.ReceiptId),
-        raw: result,
-        isVisa: fe(result.CardType, 'V'),
-        isMasterCard: fe(result.CardType, 'M'),
-        isVisaDebit: fe(result.IsVisaDebit, 'true'),
-        authCode: fe(result.AuthCode),
-        timeout: fe(result.TimedOut, 'true'),
-        date: fe(result.TransDate),
-        time: fe(result.TransTime),
-        custId: cust_id,
-        orderId: order_id,
-      };
-      const approved = !status.timeout && ((code) === '00' || code ? parseInt(code, 10) < 50 : false);
-      if (approved) {
-        return status;
-      }
-      return {
-        code: status.code,
-        msg: (status.timeout ? 'TIMEOUT' : status.msg) || 'DECLINED',
-        raw: result,
-      };
-    });
+  const response = await axios(options);
+  const xmlify = await xml.parseStringAsync(response.data);
+  const receipt = Array.isArray(xmlify.response.receipt) ? xmlify.response.receipt[0] : xmlify.response.receipt;
+  return format(receipt);
 };
+
+module.exports.init = (configuration) => {
+  config = configuration;
+  if (config.crypt_type === null) {
+    config.crypt_type = 7;
+  }
+  if (config.country_code) {
+    config.country_code = config.country_code.toUpperCase();
+    if (config.country_code !== 'CA'
+      && !Object.prototype.hasOwnProperty.call(globals, `${config.country_code}_HOST`)) {
+      return Promise.reject(new Error('Invalid country code. CA is only supported.'));
+    }
+  }
+  return Promise.resolve();
+};
+
+module.exports.resAddCC = data => send(data, 'res_add_cc');
